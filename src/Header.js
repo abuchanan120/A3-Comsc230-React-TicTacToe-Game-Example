1bc0f321be201da5c0654185b2f97357d7451493
abuchanan120 <61256045+abuchanan120@users.noreply.github.com>
2021-11-24 13:24:54 -0500

    final changes commit

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+  return (
+    
+    <div>
+       <header>
+ <div className="container">
+     <div id="branding">
+     <h1><span className="highlight">RWU Comsc230</span> Tic Tac Toe</h1>
+     </div>	
+     
+</div>
+     </header>
+    </div>
+  
+ )
+
+}
+
+export default Header
